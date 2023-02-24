a30e48e11d4784f293eccf6e759cfa60f2639f8d
Horik <h22ea@icloud.com>
2023-02-24 10:05:59 +0200

    chakra, icons, new ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@chakra-ui/button"
+import { Box, Text } from "@chakra-ui/layout"
+import { useToast } from "@chakra-ui/toast"
+import { useEffect, useState } from "react"
+import emoji from "./utils/emojis"
+import CachedRoundedIcon from '@mui/icons-material/CachedRounded'
+import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded'
+import useWindowDimensions from "./hooks/useWindowDimensions"
 
-function App() {
+const App = () => {
+  const toast = useToast()
+  const { height } = useWindowDimensions()
+
+  const [emojis, setEmojis] = useState<string>("")
+  const [reload, setReload] = useState<boolean>(false)
+
+  useEffect(() => {
+    setEmojis(`${emoji()} ${emoji()} ${emoji()} ${emoji()} ${emoji()} ${emoji()} ${emoji()} ${emoji()}`)
+  }, [reload])
+
+  const copiedToClipboard = () =>
+    toast({
+      title: `Copied ${emojis}`,
+      status: 'success',
+      duration: 9000,
+      isClosable: false,
+    })
+
+  const somethingWentWrong = () =>
+    toast({
+      title: `Something went wrong...`,
+      description: `Try reloading the website.`,
+      status: `error`,
+      duration: 9000,
+      isClosable: false,
+    })
+
+
+  const copyContent = async () => {
+    try {
+      await navigator.clipboard.writeText(emojis)
+      copiedToClipboard()
+    } catch (error) {
+      console.log('Failed to copy: ', error)
+      somethingWentWrong()
+    }
+  }
+
+  const reloadEmojis = () => setReload(!reload)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box display={"flex"} flex={1} height={height} justifyContent={"center"} alignItems={"center"} >
+
+      <Box flexDirection={"column"} >
+
+        <Text fontSize={44} fontWeight={"semibold"} color={"#F0EBEB"}>
+          {emojis}
+        </Text>
+        <Box display={"flex"} flex={1} justifyContent={"space-between"} flexDirection={"row"} marginTop={8}>
+          <Button backgroundColor={"#424242"} onClick={reloadEmojis} borderRadius={100} marginRight={3}>
+            <CachedRoundedIcon />
+          </Button>
+          <Button backgroundColor={"#424242"} flex={1} onClick={copyContent} borderRadius={100} marginLeft={3}>
+            <ContentCopyRoundedIcon />
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
-export default App;
+export default App
