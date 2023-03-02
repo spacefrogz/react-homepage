e306f9a2bd67f32510d25da57001e0995bd71fbc
Horik <h22ea@icloud.com>
2023-03-02 23:26:42 +0200

    update main view

--- src/pages/SpaceFrogsStudio.tsx
+++ src/pages/SpaceFrogsStudio.tsx
@@ -6,23 +6,26 @@ const SpaceFrogsStudio = () => {
   const { height } = useWindowDimensions()
 
   return (
-    <Box fontSize={"7xl"} fontWeight={"thin"} fontFamily={"Synkopy-Regular"} textAlign={"center"} height={height} display={"flex"} justifyContent={"center"} alignItems={"center"} >
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>S</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>p</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>a</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>c</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>e</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>F</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>r</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>o</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>g</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>z</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>S</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>t</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>u</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>d</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>i</Text>
-      <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>o</Text>
+    <Box>
+      <Box fontSize={"7xl"} fontWeight={"thin"} fontFamily={"Synkopy-Regular"} textAlign={"center"} height={height} display={"flex"} justifyContent={"center"} alignItems={"center"} >
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>S</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>p</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>a</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>c</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#aa4b6b"}>e</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>F</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>r</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>o</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>g</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#6b6b83"}>z</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>S</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>t</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>u</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>d</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>i</Text>
+        <Text _hover={{ fontFamily: "Synkopy" }} color={"#3b8d99"}>o</Text>
+      </Box>
+      <Text fontFamily={"Genos"} bgClip={"text"} bgGradient={"linear(to-l, #aa4b6b, #6b6b83, #3b8d99)"} textAlign={"center"}>Built with 💚 by Horik</Text>
     </Box>
   )
 }
