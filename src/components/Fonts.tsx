01f19f1ca080a35cd7da31bb3f2a84c34cbe9c9f
Horik <h22ea@icloud.com>
2023-03-03 00:51:53 +0200

    updated font types

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -7,7 +7,10 @@ const Fonts = () => (
         font-family: 'Synkopy-Regular';
         src: url('./fonts/Synkopy/Synkopy-Regular.eot'); /* IE9 Compat Modes */
         src: url('./fonts/Synkopy/Synkopy-Regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+             url('./fonts/Synkopy/Synkopy-Regular.woff2') format('woff2'), /* Super Modern Browsers */
+             url('./fonts/Synkopy/Synkopy-Regular.woff') format('woff'), /* Pretty Modern Browsers */
              local('Synkopy'), url('./fonts/Synkopy/Synkopy-Regular.ttf') format('truetype'); /* Safari, Android, iOS */
+             url('./fonts/Synkopy/Synkopy-Regular.svg#SynkopyRegular') format('svg');
       }
       @font-face {
         font-family: 'Synkopy-Flipside';
