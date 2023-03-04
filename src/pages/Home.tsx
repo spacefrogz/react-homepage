6fff2ebf6da5e3676066a7bfe00f7125c80d5bce
Horik <h22ea@icloud.com>
2023-03-04 10:59:49 +0200

    updates on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Zoom from 'react-reveal/Zoom'
 import { Box, Text } from '@chakra-ui/react'
 import SpaceFrogzStudio from "../components/SpaceFrogzStudio"
+import useWindowDimensions from "../hooks/useWindowDimensions"
 
 const Home = () => {
+  const { height } = useWindowDimensions()
+
   return (
     <Box>
       <SpaceFrogzStudio />
+      <Box height={height} backgroundColor={"#aa4b6b"}></Box>
+      <Box height={height} backgroundColor={"#6b6b83"}></Box>
+      <Box height={height} backgroundColor={"#3b8d99"}></Box>
       <Zoom>
-        <Text fontFamily={"GenosRoman-Medium"} fontSize={16} bgClip={"text"} bgGradient={"linear(to-r, #aa4b6b, #6b6b83, #3b8d99)"} textAlign={"center"}>Built with ❤️‍🔥 by Horik</Text>
+        <Text fontFamily={"GenosRoman-Medium"} fontSize={16} bgClip={"text"} bgGradient={"linear(to-r, #aa4b6b, #6b6b83, #3b8d99)"} textAlign={"center"}>Built with ❤️‍🔥 by Horik • 2023</Text>
       </Zoom>
     </Box>
   )
