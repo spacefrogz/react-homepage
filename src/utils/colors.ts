28dac8a63c98abf7ffaef9d9ecb145a788b827f3
Horik <h22ea@icloud.com>
2023-03-04 23:49:36 +0200

    create colors util file

---
+++ src/utils/colors.ts
@@ -0,0 +1,15 @@
+const rainbow = "#fc2847, #ffa343, #fdfc74, #71bc78, #0f4c81, #7442c8, #fb7efd"
+const rainbowGradient = "linear(to-r, #fc2847, #ffa343, #fdfc74, #71bc78, #0f4c81, #7442c8, #fb7efd)"
+
+const spaceGradient = "linear(to-r, #aa4b6b, #6b6b83, #3b8d99)"
+
+const colors = {
+  bej: "#FCEDD4"
+}
+
+export {
+  colors,
+  rainbow,
+  rainbowGradient,
+  spaceGradient
+}
