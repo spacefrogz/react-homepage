c08cf6250b738c956927dec750986ea5b7bfcd6b
Horik <h22ea@icloud.com>
2023-03-04 23:50:59 +0200

    update Home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,19 +2,20 @@ import React from 'react'
 import Zoom from 'react-reveal/Zoom'
 import { Box, Text } from '@chakra-ui/react'
 import SpaceFrogzStudio from "../components/SpaceFrogzStudio"
+import { colors, rainbowGradient } from "../utils/colors"
+import strings from "../utils/strings"
 import useWindowDimensions from "../hooks/useWindowDimensions"
 
 const Home = () => {
   const { height } = useWindowDimensions()
-
   return (
     <Box>
+      <Box height={10} bgGradient={rainbowGradient}></Box>
       <SpaceFrogzStudio />
-      <Box height={height} backgroundColor={"#aa4b6b"}></Box>
-      <Box height={height} backgroundColor={"#6b6b83"}></Box>
-      <Box height={height} backgroundColor={"#3b8d99"}></Box>
+      <Box height={10} bgGradient={rainbowGradient}></Box>
+      <Box height={height} justifyContent={"center"} alignItems={"center"} textAlign={"center"} />
       <Zoom>
-        <Text fontFamily={"GenosRoman-Medium"} fontSize={16} bgClip={"text"} bgGradient={"linear(to-r, #aa4b6b, #6b6b83, #3b8d99)"} textAlign={"center"}>Built with ❤️‍🔥 by Horik • 2023</Text>
+        <Text fontFamily={"Poppins-Regular"} fontSize={16} color={colors.bej} textAlign={"center"}>{strings.builtByHorik}</Text>
       </Zoom>
     </Box>
   )
