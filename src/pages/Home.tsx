96b61234212164f2f053778e2ed4c06b4ee0ce75
Horik <h22ea@icloud.com>
2023-03-05 20:28:19 +0200

    removed unnecessary comp from home view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,19 @@
-import React, { useState } from 'react'
-import Zoom from 'react-reveal/Zoom'
-import { Box, Text } from '@chakra-ui/react'
+import { useEffect } from 'react'
+import { Box } from '@chakra-ui/react'
 import SpaceFrogzStudio from "../components/SpaceFrogzStudio"
-import { colors } from "../utils/colors"
-import strings from "../utils/strings"
-import { Image } from "@chakra-ui/image"
-import image from "../assets/logo.png"
-import gopher from "../assets/the_gopher_frog.png"
 import Rainbow from "../components/Rainbow"
-import useCustomDimensions from "../hooks/useCustomDimensions"
+import { scrollToTop } from "../utils/behaviours"
 
 const Home = () => {
-  const { sectionH, circleLat } = useCustomDimensions()
+  useEffect(() => {
+    scrollToTop()
+  }, [])
 
-  const [imgSrc, setImgSrc] = useState(image)
   return (
     <Box>
       <Rainbow />
       <SpaceFrogzStudio />
       <Rainbow />
-      <Box display={"flex"} height={sectionH} justifyContent={"center"} alignItems={"center"}>
-        <Zoom>
-          <Image
-            width={circleLat}
-            height={circleLat}
-            src={imgSrc}
-            onClick={() => {
-              if (imgSrc === gopher) {
-                setImgSrc(image)
-              } else {
-                setImgSrc(gopher)
-              }
-            }}
-            alt="A Circle Image"
-          />
-        </Zoom>
-      </Box>
-      <Rainbow />
-      <Zoom>
-        <Text fontFamily={"Poppins-Regular"} fontSize={16} color={colors.bej} textAlign={"center"}>{strings.builtByHorik}</Text>
-      </Zoom>
     </Box>
   )
 }
